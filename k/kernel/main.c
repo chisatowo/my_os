a034fd43c6c3f6334c6b58d1712ea5eab28e8917
chisatowo <thy3972@outlook.com>
2023-12-27 17:12:08 +0800

    finishes section k : 1. add dir_read func to locate a dir_file at ones directory

--- k/kernel/main.c
+++ k/kernel/main.c
@@ -19,21 +19,29 @@ void u_prog_b(void);
 int main(void) {
    put_str("I am kernel\n");
    init_all();
-   process_execute(u_prog_a, "u_prog_a");
-   process_execute(u_prog_b, "u_prog_b");
-   thread_start("k_thread_a", 31, k_thread_a, "I am thread_a");
-   thread_start("k_thread_b", 31, k_thread_b, "I am thread_b");
+/********  测试代码  ********/
    struct dir* p_dir = sys_opendir("/dir1/subdir1");
    if (p_dir) {
-      printf("HaoYu Tan: /dir1/subdir1 open done!\n");
+      printf("/dir1/subdir1 open done!\ncontent:\n");
+      char* type = NULL;
+      struct dir_entry* dir_e = NULL;
+      while((dir_e = sys_readdir(p_dir))) { 
+	 if (dir_e->f_type == FT_REGULAR) {
+	    type = "regular";
+	 } else {
+	    type = "directory";
+	 }
+	 printf("      %s   %s\n", type, dir_e->filename);
+      }
       if (sys_closedir(p_dir) == 0) {
-	 printf("HaoYu Tan: /dir1/subdir1 close done!\n");
+	 printf("/dir1/subdir1 close done!\n");
       } else {
-	 printf("HaoYu Tan: /dir1/subdir1 close fail!\n");
+	 printf("/dir1/subdir1 close fail!\n");
       }
    } else {
-      printf("HaoYu Tan: /dir1/subdir1 open fail!\n");
+      printf("/dir1/subdir1 open fail!\n");
    }
+/********  测试代码  ********/
    while(1);
    return 0;
 }
