4b58d99e64ede10aa032e84d3ee4542374f70ec7
chisatowo <thy3972@outlook.com>
2023-12-28 09:45:14 +0800

    improves section k: modified kernel/main.c: 1. adding personal statement.

--- k/kernel/main.c
+++ k/kernel/main.c
@@ -22,7 +22,7 @@ int main(void) {
 /********  测试代码  ********/
    struct dir* p_dir = sys_opendir("/dir1/subdir1");
    if (p_dir) {
-      printf("/dir1/subdir1 open done!\ncontent:\n");
+      printf("HaoYu Tan: /dir1/subdir1 open done!\ncontent:\n");
       char* type = NULL;
       struct dir_entry* dir_e = NULL;
       while((dir_e = sys_readdir(p_dir))) { 
@@ -34,12 +34,12 @@ int main(void) {
 	 printf("      %s   %s\n", type, dir_e->filename);
       }
       if (sys_closedir(p_dir) == 0) {
-	 printf("/dir1/subdir1 close done!\n");
+	 printf("HaoYu Tan: /dir1/subdir1 close done!\n");
       } else {
-	 printf("/dir1/subdir1 close fail!\n");
+	 printf("HaoYu Tan: /dir1/subdir1 close fail!\n");
       }
    } else {
-      printf("/dir1/subdir1 open fail!\n");
+      printf("HaoYu Tan: /dir1/subdir1 open fail!\n");
    }
 /********  测试代码  ********/
    while(1);
