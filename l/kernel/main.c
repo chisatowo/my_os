45e5b0b2be9a06535d5464a638db67f9954684d9
chisatowo <thy3972@outlook.com>
2023-12-28 09:52:23 +0800

    improves section l: modified kernel/main.c: 1. adding personal statement.

--- l/kernel/main.c
+++ l/kernel/main.c
@@ -20,7 +20,7 @@ int main(void) {
    put_str("I am kernel\n");
    init_all();
 /********  测试代码  ********/
-   printf("/dir1 content before delete /dir1/subdir1:\n");
+   printf("HaoYu Tan: /dir1 content before delete /dir1/subdir1:\n");
    struct dir* dir = sys_opendir("/dir1/");
    char* type = NULL;
    struct dir_entry* dir_e = NULL;
@@ -32,25 +32,25 @@ int main(void) {
       }
       printf("      %s   %s\n", type, dir_e->filename);
    }
-   printf("try to delete nonempty directory /dir1/subdir1\n");
+   printf("HaoYu Tan: try to delete nonempty directory /dir1/subdir1\n");
    if (sys_rmdir("/dir1/subdir1") == -1) {
-      printf("sys_rmdir: /dir1/subdir1 delete fail!\n");
+      printf("HaoYu Tan: sys_rmdir: /dir1/subdir1 delete fail!\n");
    }
 
-   printf("try to delete /dir1/subdir1/file2\n");
+   printf("HaoYu Tan: try to delete /dir1/subdir1/file2\n");
    if (sys_rmdir("/dir1/subdir1/file2") == -1) {
-      printf("sys_rmdir: /dir1/subdir1/file2 delete fail!\n");
+      printf("HaoYu Tan: sys_rmdir: /dir1/subdir1/file2 delete fail!\n");
    } 
    if (sys_unlink("/dir1/subdir1/file2") == 0 ) {
-      printf("sys_unlink: /dir1/subdir1/file2 delete done\n");
+      printf("HaoYu Tan: sys_unlink: /dir1/subdir1/file2 delete done\n");
    }
    
-   printf("try to delete directory /dir1/subdir1 again\n");
+   printf("HaoYu Tan: try to delete directory /dir1/subdir1 again\n");
    if (sys_rmdir("/dir1/subdir1") == 0) {
-      printf("/dir1/subdir1 delete done!\n");
+      printf("HaoYu Tan: /dir1/subdir1 delete done!\n");
    }
 
-   printf("/dir1 content after delete /dir1/subdir1:\n");
+   printf("HaoYu Tan: /dir1 content after delete /dir1/subdir1:\n");
    sys_rewinddir(dir);
    while((dir_e = sys_readdir(dir))) { 
       if (dir_e->f_type == FT_REGULAR) {
@@ -58,7 +58,7 @@ int main(void) {
       } else {
 	 type = "directory";
       }
-      printf("      %s   %s\n", type, dir_e->filename);
+      printf("HaoYu Tan:       %s   %s\n", type, dir_e->filename);
    }
 
 /********  测试代码  ********/
